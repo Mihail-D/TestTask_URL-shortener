14e79a1dc6db9903697278bbcaa960cef0ab200e
Nuby <nuby.4register@gmail.com>
2024-07-30 12:22:01 +0300

    init

---
+++ src/test/java/ru/trial/assignments/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trial.assignments.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
