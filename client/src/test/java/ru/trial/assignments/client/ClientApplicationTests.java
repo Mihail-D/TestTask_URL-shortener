a206e4250407864013e507bc09478c5aafc2e133
Nuby <nuby.4register@gmail.com>
2024-08-15 21:54:41 +0300

    gateway-server modules

---
+++ client/src/test/java/ru/trial/assignments/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.trial.assignments.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
