d55146f4c70747d1df149737d9c3756b7119a6aa
Nuby <nuby.4register@gmail.com>
2024-11-12 09:35:29 +0300

    add: tests

--- src/test/java/ru/trial/assignments/urlshortener/link/service/LinkServiceTest.java
+++ src/test/java/ru/trial/assignments/urlshortener/link/service/LinkServiceTest.java
@@ -1,7 +1,20 @@
 package ru.trial.assignments.urlshortener.link.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class LinkServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void createShortLink() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByShortLink() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByOriginalLink() {
+    }
 
 }
